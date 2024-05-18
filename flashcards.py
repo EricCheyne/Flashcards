d6603820b0903faaaa91ec8d04a91e048b51a767
Eric Cheyne <eric.cheyne@gmail.com>
2024-05-18 16:21:27 -0700

    Updated flashcards.py

--- flashcards.py
+++ flashcards.py
@@ -1,6 +1,17 @@
-print("Card:")
+# print("Card:")
 term = input()
-print(term)
-print("Definition:")
+# print(term)
+
+# print("Definition:")
 definition = input()
-print(definition)
+# print(definition)
+
+answer = input()
+
+right_answer = "Your answer is right!"
+wrong_answer = "Your answer is wrong..."
+
+if answer == definition:
+    print(right_answer)
+elif answer != definition:
+    print(wrong_answer)
